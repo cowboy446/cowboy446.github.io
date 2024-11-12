792ecdbfea0cc2a5211139bf0e96db9d40719a9f
Cowboy Cyber <3174848262@qq.com>
2024-11-12 20:18:56 +0800

    Site updated: 2024-11-12 20:18:54

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,47 @@
+// const ap = new APlayer({
+//     container: document.getElementById('aplayer'),
+//     fixed: true,
+//     autoplay: false,
+//     audio: [
+//       {
+//         name: '风筝误',
+//         artist: '刘珂矣',
+//         url: 'http://up.mcyt.net/?down/46644.mp3',
+//         cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+//       }
+//     ]
+// });
+
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
-    audio: [
-      {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
-      }
-    ]
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  listFolded: true,
+  theme: '#FADFA3', // 主题色
+  loop: 'all',
+  order: 'list',
+  preload: 'auto', // 'none' | 'metadata' | 'auto'
+  volume: 0.7, 
+  mutex: true, // 互斥，阻止多个播放器同时播放
+  listFolded: true,
+  listMaxHeight: 90,
+  lrcType: 3, // 0: 不显示歌词, 1: 网易云歌词, 3: 普通歌词
+  audio: [
+    // {
+    //   name: '水の星へ爱をこめて',
+    //   artist: '森口博子',
+    //   url: 'http://music.163.com/song/media/outer/url?id=494858545.mp3',
+    //   cover: 'music/水の星へ爱をこめて/cover.jpg',
+    //   lrc: 'music/水の星へ爱をこめて/水の星へ愛をこめて-森口博子.lrc',
+    //   theme: '#2068bf'
+    // },
+    {
+      name: 'Midnight Blue',
+      artist: 'KISSME QUICK',
+      url: 'http://music.163.com/song/media/outer/url?id=5007568.mp3',
+      cover: 'music/MidnightBlue/cover.png',
+      lrc: 'music/MidnightBlue/MIDNIGHT BLUE-KISSME QUICK.lrc',
+    }
+  ]
 });
+
